590edaed22a009cf3075321de84d0945b2285db5
Gustavo Velazquez <gustavo.velazquezm@outlook.com>
2021-04-09 13:41:21 -0500

    Removed unused scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,43 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-  root: "./src",
-  server: {
-    port: 1234,
-  },
-  build: {
-    outDir: "../build",
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: [
-      {
-        find: "app",
-        replacement: path.resolve(__dirname, "./src/app"),
-      },
-      {
-        find: "assets",
-        replacement: path.resolve(__dirname, "./src/assets"),
-      },
-      {
-        find: "components",
-        replacement: path.resolve(__dirname, "./src/components"),
-      },
-      {
-        find: "store",
-        replacement: path.resolve(__dirname, "./src/store"),
-      },
-    ],
-  },
-});
+export default ({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+
+  return defineConfig({
+    plugins: [reactRefresh()],
+    root: "./src",
+    server: {
+      port: 1234,
+    },
+    build: {
+      outDir: "../build",
+      emptyOutDir: true,
+    },
+    define: {
+      "process.env": process.env,
+    },
+    resolve: {
+      alias: [
+        {
+          find: "app",
+          replacement: path.resolve(__dirname, "./src/app"),
+        },
+        {
+          find: "assets",
+          replacement: path.resolve(__dirname, "./src/assets"),
+        },
+        {
+          find: "components",
+          replacement: path.resolve(__dirname, "./src/components"),
+        },
+        {
+          find: "store",
+          replacement: path.resolve(__dirname, "./src/store"),
+        },
+      ],
+    },
+  });
+};
